c832029c7455297c1ef427ac729468df4214797a
Lawrence Ibarria <Ibarria@gmail.com>
2017-10-04 21:38:34 -0700

    Initialize repository and windows project

---
+++ c2.cpp
@@ -0,0 +1,12 @@
+// c2.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hello World\n");
+    return 0;
+}
+
