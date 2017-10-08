7145d501f148e384508ca500c29c1fbc77b9c71b
Lawrence Ibarria <Ibarria@gmail.com>
2017-10-08 10:32:25 -0700

    command line options

--- c2.cpp
+++ c2.cpp
@@ -1,10 +1,29 @@
 // c2.cpp : Defines the entry point for the console application.
 //
 
+#define MAJOR_V 0
+#define MINOR_V 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #undef EOF
 #include "Lexer.h"
+
+void usage()
+{
+	// show usage and version
+	printf(" c2 - experimental compiler, v%d.%d\n", MAJOR_V, MINOR_V);
+	printf("\nUsage:\n");
+	printf("c2 <source file>\n");
+}
+
+void parseOptions(int argc, char **argv)
+{
+	if (argc == 1) {
+		usage();
+		exit(0);
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -12,7 +31,9 @@ int main(int argc, char **argv)
 	Lexer lex;
 	Token t;
 
-	lex.openFile("c2.cpp");
+	parseOptions(argc, argv);
+
+	lex.openFile(argv[1]);
 
 	lex.getNextToken(t);
 
